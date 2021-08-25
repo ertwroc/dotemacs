fd86606beac6faac2c160e8a4eddefbef7f53aae
ERt <ert.wroc@gmail.com>
2021-08-25 15:56:22 +0200

    Basic packages

---
+++ custom-file.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" default))
+ '(package-selected-packages
+   '(gruvbox-theme counsel ivy-rich which-key rainbow-delimiters doom-modeline ivy company use-package lsp-ui haskell-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,81 @@
+;; Highlight current line
+(global-hl-line-mode 0)
+(global-display-line-numbers-mode t)
+(column-number-mode t)
+(add-hook 'completion-list-mode-hook (lambda () (display-line-numbers-mode 0)))
+
+;; Do not use `init.el` for `custom-*` code - use `custom-file.el` and load it
+(setq custom-file "~/.emacs.d/custom-file.el")
+(load-file custom-file)
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Add melpa repo and install use-package
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package company
+  :ensure t
+  :bind (:map company-active-map
+	      ("C-n" . company-select-next)
+	      ("C-p" . company-select-previous))
+  :config (setq company-idle-delay 0.1)
+  :init (global-company-mode))
+
+(use-package haskell-mode
+  :ensure t
+  :config
+  (setq haskell-process-type 'stack-ghci))
+
+(use-package ivy
+  :ensure t
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+ 	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :init (ivy-mode 1))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode 1)
+  :config (setq which-key-idle-delay 0.3))
+
+; There is a bug in melpa stable version https://github.com/Yevgnen/ivy-rich/issues/88
+;(use-package ivy-rich
+;  :init (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer) ; Or use command counsel-switch-buffer 
+	 :map minibuffer-local-map
+	 ("C-x C-h" . counsel-minibuffer-history)))
+
+; peach-melpa.org
+(use-package gruvbox-theme
+  :init (load-theme 'gruvbox-dark-hard t))
+
+(use-package all-the-icons)
+
