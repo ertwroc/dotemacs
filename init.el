4d1a61bb5507446a442871f566f3b0608cb9779f
ERt <ert.wroc@gmail.com>
2022-01-25 22:30:44 +0100

    Various changes

--- init.el
+++ init.el
@@ -3,6 +3,9 @@
 (global-display-line-numbers-mode t)
 (column-number-mode t)
 (add-hook 'completion-list-mode-hook (lambda () (display-line-numbers-mode 0)))
+(add-hook 'treemacs-mode-hook (lambda () (display-line-numbers-mode 0)))
+(setq-default display-fill-column-indicator-column 120)
+(add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
 
 ;; Do not use `init.el` for `custom-*` code - use `custom-file.el` and load it
 (setq custom-file "~/.emacs.d/custom-file.el")
@@ -12,10 +15,8 @@
 
 ;; Add melpa repo and install use-package
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives
-  '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
@@ -24,8 +25,34 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; = PACKAGES
+
+; == Look & feel
+
+; peach-melpa.org
+(use-package gruvbox-theme
+  :init (load-theme 'gruvbox-dark-hard t))
+
+(use-package all-the-icons)
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom
+  (doom-modeline-minor-modes t)
+  (doom-modeline-unicode-fallback t)
+  (doom-modeline-indent-info t))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; == General tools
+
+(use-package which-key
+  :init (which-key-mode 1)
+  :config (setq which-key-idle-delay 0.3))
+
+; TAB - complete matching part; RET - select; C-h - show help; C-w - go to code; C-M-s - filter candidates;
 (use-package company
-  :ensure t
   :bind (:map company-active-map
 	      ("C-n" . company-select-next)
 	      ("C-p" . company-select-previous))
@@ -38,6 +65,14 @@
   :config
   (setq haskell-process-type 'stack-ghci))
 
+(use-package magit
+  :commands magit-status
+  :custom (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package treemacs
+  )
+
+;; TOD 
 (use-package ivy
   :ensure t
   :bind (("C-s" . swiper)
@@ -55,20 +90,10 @@
 	 ("C-d" . ivy-reverse-i-search-kill))
   :init (ivy-mode 1))
 
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1))
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-(use-package which-key
-  :init (which-key-mode 1)
-  :config (setq which-key-idle-delay 0.3))
 
 ; There is a bug in melpa stable version https://github.com/Yevgnen/ivy-rich/issues/88
-;(use-package ivy-rich
-;  :init (ivy-rich-mode 1))
+(use-package ivy-rich
+  :init (ivy-rich-mode 1))
 
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
@@ -76,13 +101,10 @@
 	 :map minibuffer-local-map
 	 ("C-x C-h" . counsel-minibuffer-history)))
 
-; peach-melpa.org
-(use-package gruvbox-theme
-  :init (load-theme 'gruvbox-dark-hard t))
-
-(use-package all-the-icons)
 
 ;; LSP
+
+(use-package yasnippet)
 
 (use-package lsp-mode
   :init
@@ -94,4 +116,11 @@
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
 
+(use-package lsp-ui :commands lsp-ui-mode)
+
 (use-package lsp-haskell)
+
+(use-package flycheck
+  :init (global-flycheck-mode))
+
+
---
+++ transient/history.el
@@ -0,0 +1,5 @@
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
+ (magit-dispatch nil))
